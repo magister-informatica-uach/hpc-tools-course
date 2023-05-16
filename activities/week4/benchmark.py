8e0e15086d7c5b751746c1d17410db6e4c475476
Pablo Huijse <pablo.huijse@gmail.com>
2023-05-16 18:06:13 -0400

    make it easy to change cythonized version

--- activities/week4/benchmark.py
+++ activities/week4/benchmark.py
@@ -1,6 +1,6 @@
 from timeit import timeit
 import numpy as np
-import distancia_pares_0
+import distancia_pares_2 as distancia_pares_cython
 
 
 def distancia_pares_numpy(data):
@@ -9,10 +9,15 @@ def distancia_pares_numpy(data):
 
 if __name__ == "__main__":
     N = 1000
+    repetitions = 10
     data = np.random.randn(N, 2)
     result_numpy = distancia_pares_numpy(data)
-    time_numpy = timeit('distancia_pares_numpy(data)', setup="from __main__ import distancia_pares_numpy, data", number=3)
+    time_numpy = timeit('distancia_pares_numpy(data)',
+                        setup="from __main__ import distancia_pares_numpy, data",
+                        number=repetitions)
 
-    result_cython = distancia_pares_0.distancia_pares(data)
-    time_cython = timeit('distancia_pares_0.distancia_pares(data)', setup="import distancia_pares_0; from __main__ import data", number=3)
-    print(f"test cython 0: {np.allclose(result_numpy, result_cython)} tref:{time_numpy:0.4f} tnew:{time_cython:0.4f} SU:{time_numpy/time_cython:0.4f}")
+    result_cython = distancia_pares_cython.distancia_pares(data)
+    time_cython = timeit('distancia_pares_cython.distancia_pares(data)',
+                         setup="from __main__ import distancia_pares_cython, data",
+                         number=repetitions)
+    print(f"Numpy vs Cython <=> allclose:{np.allclose(result_numpy, result_cython)} tnumpy:{time_numpy/repetitions:0.4f} tcython:{time_cython/repetitions:0.4f} SpeedUp:{time_numpy/time_cython:0.4f} (time in secs)")
