18c34acaa3196101847df84820b8cafba2112547
Pablo Huijse <pablo.huijse@gmail.com>
2023-05-29 18:54:35 -0400

    cython openmp

--- activities/week6/benchmark.py
+++ activities/week6/benchmark.py
@@ -1,6 +1,9 @@
+import sys
+from functools import partial
 from timeit import timeit
 import numpy as np
-import distancia_pares as distancia_pares_cython
+from distancia_pares import distancia_pares as distancia_pares_seq
+from distancia_pares import distancia_pares_parallel
 
 
 def distancia_pares_numpy(data):
@@ -9,15 +12,15 @@ def distancia_pares_numpy(data):
 
 if __name__ == "__main__":
     N = 1000
+    assert len(sys.argv) == 3, "Llamar como python benchmark.py N n_jobs, donde N es la cantidad de datos y n_jobs la cantidad núcleos paralelos"
+    N, n_jobs = [int(arg) for arg in sys.argv[1:]]
     repetitions = 10
     data = np.random.randn(N, 2)
-    result_numpy = distancia_pares_numpy(data)
-    time_numpy = timeit('distancia_pares_numpy(data)',
-                        setup="from __main__ import distancia_pares_numpy, data",
-                        number=repetitions)
-
-    result_cython = distancia_pares_cython.distancia_pares(data)
-    time_cython = timeit('distancia_pares_cython.distancia_pares(data)',
-                         setup="from __main__ import distancia_pares_cython, data",
-                         number=repetitions)
-    print(f"Numpy vs Cython <=> allclose:{np.allclose(result_numpy, result_cython)} tnumpy:{time_numpy/repetitions:0.4f} tcython:{time_cython/repetitions:0.4f} SpeedUp:{time_numpy/time_cython:0.4f} (time in secs)")
+    profile = partial(timeit, globals=globals(), number=repetitions)
+    # result_numpy = distancia_pares_numpy(data)
+    # time_numpy = profile('distancia_pares_numpy(data)')
+    result_cython = distancia_pares_seq(data)
+    time_cython = profile('distancia_pares_seq(data)')
+    result_cython_par = distancia_pares_parallel(data, n_jobs)
+    time_cython_par = profile('distancia_pares_parallel(data, n_jobs)')
+    print(f"Cython Seq vs Par <=> allclose:{np.allclose(result_cython, result_cython_par)} tnumpy:{time_cython/repetitions:0.4f} tcython:{time_cython_par/repetitions:0.4f} SpeedUp:{time_cython/time_cython_par:0.4f} (time in secs)")
--- activities/week6/distancia_pares.pyx
+++ activities/week6/distancia_pares.pyx
@@ -2,14 +2,34 @@ import cython
 cimport numpy as npc
 import numpy as np
 
+from cython.parallel import prange
+
 # Por conveniencia podemos definir el tipo de data y dist con 
 ctypedef npc.float64_t TIPO_t
 TIPO = np.float64
 
-cdef extern from "math.h":
+cdef extern from "math.h" nogil:
     TIPO_t sqrt(TIPO_t)
 
 # Deshabilitamos las comprobaciones de Python:
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def distancia_pares_parallel(TIPO_t [:, ::1] data, int n_jobs=4):
+    cdef int N = data.shape[0]
+    cdef int M = data.shape[1]
+    dist = np.zeros(shape=(N, N), dtype=TIPO)
+    cdef TIPO_t [:, ::1] dist_view = dist
+    cdef Py_ssize_t i, j, k
+    with nogil:
+        for i in range(N):
+            for j in prange(i+1, N, num_threads=n_jobs):
+                for k in range(M):
+                    dist_view[i, j] += (data[i, k] - data[j, k])**2
+                dist_view[i, j] = sqrt(dist_view[i, j])
+                dist_view[j, i] = dist_view[i, j]
+    return dist
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def distancia_pares(TIPO_t [:, ::1] data):
